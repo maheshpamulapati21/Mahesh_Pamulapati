164bfa4fb540679f604d0ef0922a7f694c4ba104
maheshpamulapati <maheshpamulapati21@gmail.com>
2023-12-13 01:53:36 -0800

    Delete page

--- MY_Contacts/urls.py
+++ MY_Contacts/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.contact_list, name='contact_list'),
     path('create/', views.contact_create, name='contact_create'),
     path('<int:id>/', views.contact_detail, name='contact_detail'),
-    path('<int:id>/edit/', views.contact_edit, name='contact_update'),
+   # path('<int:id>/edit/', views.contact_edit, name='contact_update'),
+    path('edit_contact/<int:contact_id>/', views.contact_update, name='contact_update'),
     path('<int:id>/delete/', views.contact_delete, name='contact_confirm_delete'),
 ]
--- MY_Contacts/views.py
+++ MY_Contacts/views.py
@@ -1,39 +1,31 @@
-from django.shortcuts import render, redirect
-from .models import Contact  # Import your Contact model
-from .forms import ContactForm  # Import your contact form
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Contact
+from .forms import ContactForm
 
 def contact_list(request):
-    contacts = Contact.objects.all()  # Retrieve all contacts from the database
-    context = {'contact': contacts}
+    contacts = Contact.objects.all()
+    context = {'contacts': contacts}
     return render(request, 'MY_Contacts/contact_list.html', context)
 
 def contact_create(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            # Save the contact data to the database
             form.save()
-            return redirect('MY_Contacts:contact_list.html')
+            return redirect('MY_Contacts:contact_list')  # Redirect to the contact list page
     else:
         form = ContactForm()
     
     context = {'form': form}
     return render(request, 'MY_Contacts/contact_create.html', context)
 
-from django.shortcuts import render, get_object_or_404
-
 def contact_detail(request, id):
-    contact = get_object_or_404(Contact, id=id)  # Retrieve the contact by its ID
-    context = {'contact': contacts}
+    contact = get_object_or_404(Contact, id=id)
+    context = {'contact': contact}
     return render(request, 'MY_Contacts/contact_detail.html', context)
 
-from django.shortcuts import render, redirect, get_object_or_404
-from .models import Contact  # Import your Contact model
-from .forms import ContactForm  # Import your contact form
-
 def contact_edit(request, id):
-    contact = get_object_or_404(Contact, id=id)  # Retrieve the contact by its ID
-
+    contact = get_object_or_404(Contact, id=id)
     if request.method == 'POST':
         form = ContactForm(request.POST, instance=contact)
         if form.is_valid():
@@ -42,18 +34,27 @@ def contact_edit(request, id):
     else:
         form = ContactForm(instance=contact)
     
-    context = {'form': form, 'contact': contacts}
+    context = {'form': form, 'contact': contact}
     return render(request, 'MY_Contacts/contact_update.html', context)
 
-from django.shortcuts import render, redirect, get_object_or_404
-from .models import Contact  # Import your Contact model
-
 def contact_delete(request, id):
-    contact = get_object_or_404(Contact, id=id)  # Retrieve the contact by its ID
-
+    contact = get_object_or_404(Contact, id=id)
     if request.method == 'POST':
         contact.delete()
-        return redirect('MY_Contacts:contact_list')  # Redirect to the contact list
-    
-    context = {'contact': contacts}
-    return render(request, 'MY_Contacts/contact_confirm_delete.html', context)
+        return redirect('MY_Contacts:contact_list')  # Redirect to the contact list page
+    else:
+        context = {'contact': contact}
+        return render(request, 'MY_Contacts/contact_confirm_delete.html', context)
+
+def contact_update(request, contact_id):
+    contact = get_object_or_404(Contact, pk=contact_id)
+
+    if request.method == 'POST':
+        # Handle form submission and update the contact
+        contact.first_name = request.POST['first_name']
+        contact.last_name = request.POST['last_name']
+        contact.phone_number = request.POST['phone_number']
+        contact.save()
+        return redirect('MY_Contacts:contact_list')  # Redirect to the contact list page after editing
+
+    return render(request, 'MY_Contacts/contact_update.html', {'contact': contact})
